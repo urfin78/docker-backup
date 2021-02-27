16e5bd32b27cebbb1ab848434f2640889848d23c
urfin78 <urfin78@gmail.com>
2021-02-27 18:43:05 +0100

    changed from bz2 to gz for mysql image init

--- docker-mysqldump.sh
+++ docker-mysqldump.sh
@@ -26,7 +26,7 @@ for CONTAINER in $(docker ps --format '{{.ID}}'); do
 			PREPAREENV+='DBPW='${DBPW}';'
 		fi
 
-		docker exec ${CONTAINER} /bin/bash -c ''"${PREPAREENV}"' mysqldump --single-transaction -n -c -e --hex-blob -R -u ${DBUSER} -p${DBPW} ${DB}' | bzip2 -c > ${TARGETDIR}/${DBASE}-${DATE}.sql.bz2
+		docker exec ${CONTAINER} /bin/bash -c ''"${PREPAREENV}"' mysqldump --single-transaction -n -c -e --hex-blob -R -u ${DBUSER} -p${DBPW} ${DB}' | gzip -9 -c > ${TARGETDIR}/${DBASE}-${DATE}.sql.gz
 		PREPAREENV=''
 	fi 
 done
