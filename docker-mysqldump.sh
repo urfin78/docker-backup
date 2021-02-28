2534e6b96dd3e8b4bb7e6eb76c23e64ea5d0f383
urfin78 <urfin78@gmail.com>
2021-02-28 07:39:41 +0100

    fix shellcheck issues

--- docker-mysqldump.sh
+++ docker-mysqldump.sh
@@ -3,24 +3,26 @@ DATE=$(date +"%Y%m%d-%H%M%S")
 TARGETDIR='.'
 PREPAREENV=''
 for CONTAINER in $(docker ps --format '{{.ID}}'); do
-	ENVDATA=$(docker inspect --format '{{.Config.Env}}' ${CONTAINER})
+	ENVDATA=$(docker inspect --format '{{.Config.Env}}' "${CONTAINER}")
 	if echo "${ENVDATA}"|grep -qoE 'MYSQL_DATABASE(|_FILE)=.*'; then
 		DB=$(echo "${ENVDATA}"|grep -oE 'MYSQL_DATABASE(|_FILE)=.*'|sed -E 's/^MYSQL_DATABASE(|_FILE)=([^ ]+).*/\2/')
 		DBUSER=$(echo "${ENVDATA}"|grep -oE 'MYSQL_USER(|_FILE)=.*'|sed -E 's/^MYSQL_USER(|_FILE)=([^ ]+).*/\2/')
 		DBPW=$(echo "${ENVDATA}"|grep -oE 'MYSQL_PASSWORD(|_FILE)=.*'|sed -E 's/^MYSQL_PASSWORD(|_FILE)=([^ ]+).*/\2/') 
-		if $(echo "${ENVDATA}"|grep -qoE 'MYSQL_DATABASE_FILE'); then
+		if echo "${ENVDATA}"|grep -qoE 'MYSQL_DATABASE_FILE'; then
 			PREPAREENV+='DB=$(cat '${DB}');'
 			DBASE=$(docker exec "${CONTAINER}" /bin/bash -c ''"${PREPAREENV}"' echo ${DB}')
 		else
 			PREPAREENV+='DB='${DB}';'
 			DBASE="${DB}"
 		fi
-		if echo ${ENVDATA}|grep -qoE 'MYSQL_USER_FILE'; then
+		if echo "${ENVDATA}"|grep -qoE 'MYSQL_USER_FILE'; then
+			#shellcheck disable=SC2016
 			PREPAREENV+='DBUSER=$(cat '${DBUSER}');'
 		else
 			PREPAREENV+='DBUSER='${DBUSER}';'
 		fi
-		if echo ${ENVDATA}|grep -qoE 'MYSQL_PASSWORD_FILE'; then
+		if echo "${ENVDATA}"|grep -qoE 'MYSQL_PASSWORD_FILE'; then
+			#shellcheck disable=SC2016
 			PREPAREENV+='DBPW=$(cat'" ${DBPW}"'); '
 		else
 			PREPAREENV+='DBPW='${DBPW}';'
